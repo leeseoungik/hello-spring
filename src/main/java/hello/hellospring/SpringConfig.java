128d303090799d4ff091940dfb66d8d6f765ea29
silee <leeseoungik@gmail.com>
2022-06-26 19:39:52 +0900

    fisrst commit

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,38 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+@Configuration
+public class SpringConfig {
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    private final MemberRepository memberRepository;
+
+    //@Autowired   //생성자가 1개면 @Autowired 생략가능
+    public SpringConfig(DataSource dataSource, EntityManager em, MemberRepository memberRepository) {
+
+        this.dataSource = dataSource;
+        this.em = em;
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+}
